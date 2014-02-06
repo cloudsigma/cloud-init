74fc38d9fec62a2efe92327209b2799405011c63
Kiril Vladimirov <kiril@vladimiroff.org>
2014-02-06 17:29:13 +0200

    Use cepko instead of embedding it and add useful docstrings

--- cloudinit/sources/DataSourceCloudSigma.py
+++ cloudinit/sources/DataSourceCloudSigma.py
@@ -16,21 +16,31 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 from slugify import slugify
+from cepko import Cepko
 
 from cloudinit import log as logging
 from cloudinit import sources
-from cloudinit import cs_utils
 from cloudinit import util
 
 LOG = logging.getLogger(__name__)
 
 
 class DataSourceCloudSigma(sources.DataSource):
+    """
+    Uses cepko in order to gather the server context from the VM.
+
+    For more information about CloudSigma's Server Context:
+    http://cloudsigma-docs.readthedocs.org/en/latest/server_context.html
+    """
     def __init__(self, sys_cfg, distro, paths):
-        self.cepko = cs_utils.Cepko()
+        self.cepko = Cepko()
         sources.DataSource.__init__(self, sys_cfg, distro, paths)
 
     def get_data(self):
+        """
+        Metadata is the whole server context and /meta/cloud-config is used
+        as userdata.
+        """
         try:
             server_context = self.cepko.all().result
             server_meta = server_context['meta']
@@ -43,6 +53,10 @@ class DataSourceCloudSigma(sources.DataSource):
         return True
 
     def get_hostname(self, fqdn=False):
+        """
+        Cleans up and uses the server's name if the latter is set. Otherwise
+        the first part from uuid is being used.
+        """
         if len(self.metadata['name']) > 0:
             return slugify(self.metadata['name'])[:61]
         else:
@@ -52,7 +66,8 @@ class DataSourceCloudSigma(sources.DataSource):
         return [self.ssh_public_key]
 
 
-# Used to match classes to dependencies
+# Used to match classes to dependencies. Since this datasource uses the serial
+# port network is not really required, so it's okay to load without it, too.
 datasources = [
     (DataSourceCloudSigma, (sources.DEP_FILESYSTEM)),
     (DataSourceCloudSigma, (sources.DEP_FILESYSTEM, sources.DEP_NETWORK)),
